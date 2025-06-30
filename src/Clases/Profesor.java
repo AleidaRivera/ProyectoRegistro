1fdce139e358c46787512f8beaa7ca24c184feed
Aleida Rivera <aleidarivera306@gmail.com>
2025-06-29 20:28:13 -0600

    Clase Profesor Modificada y Lista

--- src/Clases/Profesor.java
+++ src/Clases/Profesor.java
@@ -1,9 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Clases;
-
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author RODRIGUEZ
@@ -12,13 +12,33 @@ public class Profesor extends Persona {
      
     private String idProfesor;
     private String especialidad;
-    private Persona.TipoContrato tipoContrato;
+    private String tipoContrato;
 
-    public Profesor(String idProfesor, String especialidad, Persona.TipoContrato tipoContrato, String nombres, String apellidos, String email, String telefono) {
+    public Profesor(String idProfesor, String especialidad, String tipoContrato, String nombres, String apellidos, String email, String telefono) {
         super(nombres, apellidos, email, telefono);
         this.idProfesor = idProfesor;
         this.especialidad = especialidad;
         this.tipoContrato = tipoContrato;
+        
+        
+         //System.out.print("entro aca!\n");
+         File myObj = new File("C:\\Archivos\\profesor.txt");                                         
+                     
+        try {
+            FileWriter escritor = new FileWriter(myObj, true); // true para agregar (append)  
+            escritor.write("Id: " + idProfesor + " | ");
+            escritor.write(nombres + " | "); 
+            escritor.write(apellidos + " | ");
+            escritor.write(email + " | ");
+            escritor.write(telefono + " | ");
+            escritor.write(especialidad + " | ");
+            escritor.write(tipoContrato + " |\n");
+            System.out.print("Profesor Agregado al Sistema!\n");
+            escritor.close();       
+        } catch (IOException ex) {
+            Logger.getLogger(Estudiante.class.getName()).log(Level.SEVERE, null, ex);
+        }  
+        
     }
 
     public String getIdProfesor() {
@@ -37,14 +57,7 @@ public class Profesor extends Persona {
         this.especialidad = especialidad;
     }
 
-    public Persona.TipoContrato getTipoContrato() {
-        return tipoContrato;
-    }
-
-    public void setTipoContrato(Persona.TipoContrato tipoContrato) {
-        this.tipoContrato = tipoContrato;
-    }
-    
+         
      @Override
     public String toString() {
         return "Profesor: " + getNombreCompleto() + " - " + especialidad;
